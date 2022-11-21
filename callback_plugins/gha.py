dd3f01338a825378ae1747da4659a82d621b7aae
Ste <steff.bpoulsen@gmail.com>
2022-11-21 21:02:48 +0100

    Refactor plugin to simplify logic

--- callback_plugins/gha.py
+++ callback_plugins/gha.py
@@ -26,7 +26,7 @@ DOCUMENTATION = '''
         - section: callback_gha
           key: group_per_play
       vars:
-        - name: ansible_callback_diy_on_any_msg
+        - name: ansible_callback_gha_group_per_play
       type: bool
 '''
 
@@ -43,10 +43,14 @@ class CallbackModule(Default):
     CALLBACK_NAME = 'gha'
 
     def _display_end_group(self):
-        self._display.display('::endgroup::')
+        if self.print_end_group:
+            self._display.display('::endgroup::')
+            self.print_end_group = False
 
     def _display_group(self, title):
-        self._display.display(("::group::%s" % (title)))
+        if self.print_group:
+            self._display.display(("::group::%s" % (title)))
+            self.print_end_group = True
     
     def __init__(self):
         self.print_group = ('CI' in os.environ)
@@ -55,15 +59,9 @@ class CallbackModule(Default):
         super(CallbackModule, self).__init__()
 
     def v2_playbook_on_play_start(self, play):
-        if self.print_end_group:
-            self._display_end_group()
-            self.print_end_group = False
-            
+        self._display_end_group()
         if self.get_option("group_per_play"):
-            if self.print_group:
-                self.print_end_group = True
-                self._display_group("PLAY [%s]" % (play.name))
-
+            self._display_group("PLAY [%s]" % (play.name))
         super(CallbackModule, self).v2_playbook_on_play_start(play)
 
     def v2_playbook_on_task_start(self, task, is_conditional):
@@ -71,18 +69,15 @@ class CallbackModule(Default):
             role_name = task._role.get_name(include_role_fqcn=True) if task._role else ""
 
             if self.last_role != role_name:
-                if self.last_role != "" and self.print_end_group:
+                if self.last_role != "":
                     self._display_end_group()
-                    self.print_end_group = False
 
-                if self.print_group and role_name != "":
+                if role_name != "":
                     self._display_group("ROLE [%s]" % (role_name))
-                    self.print_end_group = True
-                self.last_role = role_name
 
+                self.last_role = role_name
         super(CallbackModule, self).v2_playbook_on_task_start(task, is_conditional)
 
     def v2_playbook_on_stats(self, stats):
-        if self.print_end_group:
-            self._display_end_group()
+        self._display_end_group()
         super(CallbackModule, self).v2_playbook_on_stats(stats)
